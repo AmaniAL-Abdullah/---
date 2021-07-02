8e697bc2a60e4f24c1cb07cce98669172f90bab5
AmaniAL-Abdullah <79908839+AmaniAL-Abdullah@users.noreply.github.com>
2021-07-03 02:22:43 +0300

    Create sing.php

---
+++ sing.php
@@ -0,0 +1,51 @@
+
+# 
+<html>
+<head>
+<title> موقع رعب مداوي </ title>
+</head>
+<body style = " direction: rtl؛ text-align: right؛ background-color: # bb2222؛ font-weight: bold؛ " >
+
+
+
+<form action = " print.php " method = " post " >
+   الإسم <br>
+  <نوع الإدخال = " text " name = " name " > <br>
+   🔐 الرقم السري <br>
+  <input type = " password " name = " password " > <br>
+   📩 البريد <br>
+  <input type = " email " name = " email " > <br>
+   📞 رقم الجوال <br>
+  <input type = " text " name = " mobile " maxlength = " 12 " > <br>
+   تاريخ الميلاد <br>
+  <نوع الإدخال = " text " name = " PIOW " maxlength = " 12 " > <br>
+    <br>
+  <input type = " text " name = " PIOW " value = " " للقراءة فقط>
+<br> <br>
+
+<br>
+  <input type = " radio " name = " gender " value = " male " محددة> <br>
+  <input type = " radio " name = " gender " value = " female " > <br>
+<br> <br>
+ العمر
+<br>
+  <input type = " radio " name = " gender " value = " male " محددة> <br>
+  <input type = " radio " name = " gender " value = " female " >
+  <br>
+<br> <br>
+<textarea name = " message " rows = " 10 " cols = " 30 " >
+رسالة رعب مداوي
+</textarea>
+<br> <br>
+☠️ أي 
+الخدمات تفضل؟
+<br>
+  <input type = " checkbox " name = " lang1 " value = " python " > مساجد <br>
+  <input type = " checkbox " name = " lang2 " value = " php " > تبرع <br>
+<br> <br>
+<button action = " submit " > التسجيل </ button>
+</form>
+<مركز> <a style= "text-decoration: none؛color:black؛font-size:14px؛"   target= "_blank"> IMG SRC = < " ho.jpeg " العرض = " 50px " > <BR>
+- جرعة الرعب - </a> </center>
+</body>
+</html>

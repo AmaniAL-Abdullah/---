32ada201b74f41106579e189aa9b90050479b6c9
AmaniAL-Abdullah <79908839+AmaniAL-Abdullah@users.noreply.github.com>
2021-07-03 02:21:39 +0300

    Create login-code.php

---
+++ login-code.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title> - Login</title>
+  </head>
+<body>
+<body style="direction:rtl; text-align:right; background-color:#bb2222;font-weight:bold;">
+<form action="login.php" method="post">
+📩 Email:    <input type="text" name="email">
+<br>
+🔐 Passw: <input type="password" name="pass">
+<br><br>
+<input type="submit" value="دخول ">
+</form>
+<center><a style="text-decoration: none;color:black;font-size:14px;" href="https://techcampus.com/bootcamp?piow" target="_blank"><br>
+ </a></center>
+</body>
+</html>

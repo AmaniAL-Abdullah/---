cbfa2b4944f0bf07caef4b925700ffdba6804322
AmaniAL-Abdullah <79908839+AmaniAL-Abdullah@users.noreply.github.com>
2021-07-03 19:15:12 +0300

    Create login--code.php

---
+++ login--code.php
@@ -0,0 +1,18 @@
+
+<html>
+  <head>
+    <title> - Login</title>
+  </head>
+<body>
+<body style="direction:rtl; text-align:right; background-color:#908269;font-weight:bold;">
+<form action="login.php" method="post">
+📞 جوال    <input type="text" name="email">
+<br>
+🔐 رمز الدخول <input type="password" name="pass">
+<br><br>
+<input type="submit" value="دخول ">
+</form>
+<center><a style="text-decoration: none;color:black;font-size:14px;" href="https://techcampus.com/bootcamp?piow" target="_blank"><br>
+ </a></center>
+</body>
+</html>
